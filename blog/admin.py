2afe4051895dcf6f02ed150f273fb270cfd6b2cb
amirajoodani <amir.ajoodani@gmail.com>
2021-11-28 17:11:45 +0330

    template

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register (UserProfile)
-admin.site.register (Article)
-admin.site.register (category)
+#admin.site.register (UserProfile)
+#admin.site.register (Article)
+#admin.site.register (category)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['User', 'avatar', 'description']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ['title', 'category', 'created_at']
+
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover']
+
+
+admin.site.register(category, CategoryAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class IndexPage(TemplateView):
+    template_name="index.html"
